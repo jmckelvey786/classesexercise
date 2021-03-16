1edb04f6b145f8b7e030a83d9d97bb61f9d3150a
jmckelvey786 <mckelveywarner@gmail.com>
2021-03-15 21:15:54 -0400

    initial commit

---
+++ ClassesExercises/Car.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassesExercises
+{
+    class Car
+    {
+        public string Make;
+        public string Model;
+        public int Year;
+    }
+}
---
+++ ClassesExercises/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ClassesExercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Car!");
+            Car car1 = new Car();
+            car1.Year = 2011;
+            car1.Make = "Ford";
+            car1.Model = "Fiesta";
+
+            Console.WriteLine(car1.Year);
+            Console.WriteLine(car1.Make);
+            Console.WriteLine(car1.Model);
+        }
+    }
+}
